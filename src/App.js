1c65ca191f856400c5ab93b39d77301e805da38c
asifhossain <asifhossain976@gmail.com>
2021-08-10 00:08:55 +0600

    Registration Done with API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
-import './style.css';
+import './admin_style.css';
 
 import { Redirect } from 'react-router';
 import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
@@ -10,6 +10,7 @@ import Signup from './admin_components/signup';
 import Sidemenu from './admin_components/Layouts/sidemenu';
 import Dashboard from './admin_components/dashbord';
 import AddUser from './admin_components/addUser';
+import Notfound404 from './admin_components/notfound404';
 function App() {
   return (
     <Router>
@@ -21,12 +22,10 @@ function App() {
         <Route exact path="/signup" component={Signup} />
         {/* <Route exact path="/dashboard" component={Sidemenu} /> */}
       </Switch>
-
-      <Sidemenu />
-
       <Switch>
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/adduser" component={AddUser} />
+        <Route component={Notfound404} />
       </Switch>
 
     </Router>
